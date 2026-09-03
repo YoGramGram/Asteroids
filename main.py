004403b65443c13b75b99e844f90ecdb62fc7c60
epleycole <epleycole@gmail.com>
2026-09-03 13:08:43 -0500

    stabilized framerate

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from logger import log_state
 
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}.")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0.0
 	while True:
 		log_state()
-	for event in pygame.event.get():
-		pass
-	pygame.Surface.fill("black")
-	display.flip()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			return
+		dt = clock.tick(60) / 1000
+		screen.fill("black")
+		pygame.display.flip()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 
 if __name__ == "__main__":
     main()
