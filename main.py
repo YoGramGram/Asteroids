05f88a7f45638d65b582da72cbe4c65443fdd705
epleycole <epleycole@gmail.com>
2026-09-03 14:44:13 -0500

    player movement and update and drawable refactor

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, LINE_WIDTH
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
-def main():
+def main() -> None:
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	dt = 0.0
+
 
 	while True:
 		log_state()
-		screen.fill("black")
-		player.draw(screen)
-		player.update(dt)
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000
+
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
+                        if event.type == pygame.QUIT:
+                                return
+
+		updatable.update(dt)
+
+		screen.fill("black")
+
+		for obj in drawable:
+			obj.draw(screen)
+
+		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
