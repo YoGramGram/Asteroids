8b0f204be06e23065a4254965c5f47a34ebf6f98
epleycole <epleycole@gmail.com>
2026-09-05 18:26:34 -0500

    rate limited player shooting

--- player.py
+++ player.py
@@ -1,23 +1,24 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
 	def __init__(self, x: float, y: float):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.shoot_cooldown = 0
 
 	def draw(self, screen: pygame.Surface) -> None:
 		pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
 	def triangle(self) -> list[pygame.Vector2]:
-                forward = pygame.Vector2(0, 1).rotate(self.rotation)
-                right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-                a = self.position + forward * self.radius
-                b = self.position - forward * self.radius - right
-                c = self.position - forward * self.radius + right
-                return [a, b, c]
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+		a = self.position + forward * self.radius
+		b = self.position - forward * self.radius - right
+		c = self.position - forward * self.radius + right
+		return [a, b, c]
 
 	def rotate(self, dt: float) -> None:
 		self.rotation += PLAYER_TURN_SPEED * dt
@@ -29,6 +30,7 @@ class Player(CircleShape):
 		self.position += rotated_with_speed_vector
 
 	def update(self, dt: float) -> None:
+		self.shoot_cooldown -= dt
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_a]:
@@ -40,7 +42,11 @@ class Player(CircleShape):
 		if keys[pygame.K_s]:
 			self.move(-dt)
 		if keys[pygame.K_SPACE]:
-			self.shoot()
+			if self.shoot_cooldown > 0:
+				pass
+			else:
+				self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+				self.shoot()
 
 	def shoot(self) -> None:
 		shot = Shot(self.position.x, self.position.y)
