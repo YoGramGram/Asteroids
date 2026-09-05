d08cee23f25c491e4bc758fc6e1c4652e639f995
epleycole <epleycole@gmail.com>
2026-09-05 15:27:24 -0500

    fixed asteroid spawnrate, player now shoots

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
+
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
 
 def main() -> None:
 	pygame.init()
@@ -10,14 +15,19 @@ def main() -> None:
 
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	Shot.containers = (shots,updatable, drawable)
+	AsteroidField.containers = updatable
+	asteroid_field = AsteroidField()
 
 	Player.containers = (updatable, drawable)
-
 
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	dt = 0.0
-
 
 	while True:
 		log_state()
@@ -27,6 +37,12 @@ def main() -> None:
                                 return
 
 		updatable.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.collides_with(player):
+				log_event("player_hit")
+				print("Game over!")
+				sys.exit()
 
 		screen.fill("black")
 
